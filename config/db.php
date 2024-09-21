7f0df587284cf598ac78266bc0c48b6437ee28b9
Eduardo87h7 <eduardogarciar160998@gmail.com>
2024-09-21 10:54:12 -0600

    prueba deploy 1

--- config/db.php
+++ config/db.php
@@ -1,12 +1,12 @@
 <?php
-// Variables de entorno para Railway
-$host = getenv('DB_HOST');
-$db = getenv('DB_NAME');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
+$host = getenv('DB_HOST');  // Obteniendo el host desde la variable de entorno
+$db = getenv('DB_NAME');    // Obteniendo el nombre de la base de datos
+$user = getenv('DB_USER');  // Obteniendo el usuario desde la variable de entorno
+$pass = getenv('DB_PASS');  // Obteniendo la contraseña desde la variable de entorno
+$port = getenv('DB_PORT') ?: '5432'; // Usando 5432 como puerto predeterminado si no se configura
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Error de conexión: ' . $e->getMessage();
