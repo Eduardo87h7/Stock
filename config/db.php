1eced7b52d1a74dd087a0f0c92922d0ced33f1d0
Eduardo87h7 <eduardogarciar160998@gmail.com>
2024-09-21 14:21:52 -0600

    test Deploy 2

--- config/db.php
+++ config/db.php
@@ -6,21 +6,10 @@ $pass = getenv('PGPASSWORD');  // Obteniendo la contraseña desde la variable de
 $port = getenv('PGPORT') ?: '5432'; // Usando 5432 como puerto predeterminado si no se configura
 
 try {
-    // Conectar a la base de datos PostgreSQL en Railway
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // Modificar la clave foránea en la tabla reportes
-    $sqlDropConstraint = "ALTER TABLE reportes DROP CONSTRAINT reportes_producto_id_fkey";
-    $pdo->exec($sqlDropConstraint);
-
-    $sqlAddConstraint = "ALTER TABLE reportes ADD CONSTRAINT reportes_producto_id_fkey FOREIGN KEY (producto_id) REFERENCES products(id) ON DELETE CASCADE";
-    $pdo->exec($sqlAddConstraint);
-
-    echo "Clave foránea modificada para usar ON DELETE CASCADE exitosamente.<br>";
-
 } catch (PDOException $e) {
-    // En caso de error, mostrar mensaje
     echo 'Error de conexión: ' . $e->getMessage();
+    exit();
 }
 ?>
