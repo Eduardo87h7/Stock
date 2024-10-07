038d4e0a26ee0c9587bc85f642e8895610ed1181
Eduardo87h7 <82614756+Eduardo87h7@users.noreply.github.com>
2024-10-07 14:55:06 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,11 +4,12 @@ $db = getenv('PGDATABASE');    // Obteniendo el nombre de la base de datos
 $user = getenv('PGUSER');  // Obteniendo el usuario desde la variable de entorno
 $pass = getenv('PGPASSWORD');  // Obteniendo la contraseña desde la variable de entorno
 $port = getenv('PGPORT') ?: '5432'; // Usando 5432 como puerto predeterminado si no se configura
+
 try {
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo 'Error de conexión: ' . $e->getMessage();
     exit();
-}
+   } 
 ?>
