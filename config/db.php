3c60b68d5d6a6ce53c55c744359da9a092b163ca
Eduardo87h7 <82614756+Eduardo87h7@users.noreply.github.com>
2024-12-13 19:22:08 -0600

    Update db.php

--- config/db.php
+++ config/db.php
@@ -4,13 +4,10 @@ $db = getenv('PGDATABASE');    // Obteniendo el nombre de la base de datos
 $user = getenv('PGUSER');  // Obteniendo el usuario desde la variable de entorno
 $pass = getenv('PGPASSWORD');  // Obteniendo la contraseña desde la variable de entorno
 $port = getenv('PGPORT') ?: '5432'; // Usando 5432 como puerto predeterminado si no se configura
-
-
 try {
     // Conectar a la base de datos PostgreSQL en Railway
     $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // Crear la tabla usuarios
     $sqlUsuarios = "CREATE TABLE IF NOT EXISTS usuarios (
                         id SERIAL PRIMARY KEY,
